21e180628f1a389c5cf0a00d75c057f54f303378
jesus <macbookpro@MacBook-Pro.local>
2021-11-23 18:18:56 -0600

    stake and unstake

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,14 +3,15 @@ const DaiToken = artifacts.require('DaiToken');
 const TokenFarm = artifacts.require('TokenFarm');
 
 module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock DAI Token
   await deployer.deploy(DaiToken);
   const daiToken = await DaiToken.deployed();
 
   await deployer.deploy(DappToken);
   const dappToken = await DappToken.deployed();
 
-  await deployer.deploy(TokenFarm, DappToken.address, DaiToken.address);
-  const tokenFarm = await DappToken.deployed();
+  await deployer.deploy(TokenFarm, daiToken.address, dappToken.address);
+  const tokenFarm = await TokenFarm.deployed();
 
   await dappToken.transfer(tokenFarm.address, '1000000000000000000000000');
 
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,9 +7,55 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
+
+    address[] public stakers;
+    mapping(address => uint256) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
 
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
+    }
+
+    // stake tokens
+    function stakeTokens(uint256 _amount) public {
+        require(_amount > 0, "amount cannot be 0");
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if (!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+        hasStaked[msg.sender] = true;
+        isStaking[msg.sender] = true;
+    }
+
+    // unstake tokens
+    function unStake() public {
+        uint256 balance = stakingBalance[msg.sender];
+
+        require(balance > 0, "staking balance cannot be 0");
+
+        daiToken.transfer(msg.sender, balance);
+
+        stakingBalance[msg.sender] = 0;
+
+        isStaking[msg.sender] = false;
+    }
+
+    // Issue tokens
+    function issueTokens() public {
+        require(msg.sender == owner, "caller must be the owner");
+        for (uint256 i = 0; i < stakers.length; i++) {
+            address recepient = stakers[i];
+            uint256 balance = stakingBalance[recepient];
+            if (balance > 0) {
+                dappToken.transfer(recepient, balance);
+            }
+        }
     }
 }
